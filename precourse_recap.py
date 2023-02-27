fd68289b6c5fa776f2fa1ccf298081c2989fb3ec
KatWallen <lewisbrennan444@gmail.com>
2023-02-27 19:53:59 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+day_of_week = "Monday"
+current_week = 1
+current_day_of_week = 1
+
+# call the print function using the variables and some operators
+# we also use the str() function to convert integers to strings
+
+print("Today is " + day_of_week + ", Week " + str(current_week) 
+      + " Day " + str(current_day_of_week) + " of CodeClan")
